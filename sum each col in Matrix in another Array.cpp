dd82ef972d27bff019723a24475828fc83a03065
ahmed lahmidi <ahmedlahmidigit@gmail.com>
2026-04-25 19:00:35 +0100

    problem 05 : sum each col in Matrix in another Array

---
+++ sum each col in Matrix in another Array.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include<iomanip>
+#include"MyLib.h"
+using namespace std;
+
+void FillMatrixWithRandomNumbers(int Arr[3][3], short Rows, short Cols)
+{
+
+
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            Arr[i][j] = MyLib::RandomNumber(1, 100);
+        }
+    }
+}
+
+void OutPutMessage()
+{
+    cout << "The follwing is a 3x3 random matrix: \n";
+}
+void OutPutMessage2()
+{
+    cout << "The sum of each row in the matrix\n";
+}
+void PrintMatrix(int Arr[3][3], short Rows, short Cols)
+{
+    OutPutMessage();
+
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+
+            cout << setw(3) << Arr[i][j] << "    ";
+
+        }
+        cout << endl;
+    }
+}
+
+int ColsSum(int Arr[3][3], short Rows, short NumberCols)
+{
+
+    int Sum = 0;
+    for (int j = 0; j < Rows; j++)
+    {
+        Sum += Arr[j][NumberCols];
+    }
+    return Sum;
+}
+
+void SumOfEachColsInTheMatrixIn_Array(int Arr[3][3], int arr[3], short Rows, short Cols)
+{
+    for (int i = 0; i < Cols; i++)
+    {
+        arr[i] = ColsSum(Arr, Rows, i);
+    }
+}
+void PrintSumOfEachColsInTheMatrixIn_Array(int arr[3], short Length)
+{
+    OutPutMessage2();
+    for (int i = 0; i <= Length - 1; i++)
+    {
+        cout << "Cols " << i + 1 << " sum = " << arr[i] << endl;
+    }
+
+}
+
+
+void StartProgram()
+{
+    srand((unsigned)time(NULL));
+    int Arr[3][3];
+    int arr[3];
+
+
+    FillMatrixWithRandomNumbers(Arr, 3, 3);
+    PrintMatrix(Arr, 3, 3);
+
+    SumOfEachColsInTheMatrixIn_Array(Arr, arr, 3, 3);
+    PrintSumOfEachColsInTheMatrixIn_Array(arr, 3);
+}
+
+int main()
+{
+    StartProgram();
+
+    return 0;
+}
+
+
